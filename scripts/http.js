b67042b3aed268ef1a7d2ccb3208f9fc0930bc4c
js <54337467@qq.com>
2022-01-19 20:37:31 +0800

    '视频列表-分页'

--- scripts/http.js
+++ scripts/http.js
@@ -4,3 +4,9 @@ const http = axios.create({
   baseURL: 'http://82.156.8.100:9001/api/v1',
   timeout: 10000
 })
+
+
+const httpV2 = axios.create({
+  baseURL: 'http://82.156.8.100:9001/api/v2',
+  timeout: 10000
+})
--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,33 @@ template.defaults.imports.relativeTime = function (value) {
 }
 
 // 1. 发送请求，获取视频列表
-function loadData() {
-  http
-    .get('/videos')
+function loadData(pagenum, pagesize) {
+  httpV2
+    .get(`/videos?pagenum=${pagenum}&pagesize=${pagesize}`)
     .then(res => {
-      const { success, data } = res.data
+      const { success, data: { count, rows } } = res.data
       if (success) {
         // 渲染模板
         const html = template('tpl', {
-          videos: data
+          videos: rows
         })
         $('.videos').html(html)
+
+        // 设置分页条
+        $('.pager').pager({
+          // 页码 从0开始
+          pageIndex: pagenum - 1,
+          // 每页显示多少条
+          pageSize: pagesize,
+          // 总共的数据条数
+          itemCount: count,
+          // 除去第一页和最后一页的最大按钮数量
+          maxButtonCount: 7,
+          // 当页码发生改变的时候执行
+          onPageChanged: function(page) {
+            loadData(page + 1, pagesize)
+          }
+        })
       } else {
         // 提示
         Toastify({
@@ -29,4 +45,4 @@ function loadData() {
     })
 }
 
-loadData()
+loadData(1, 3)
