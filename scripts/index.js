117fee28119bb8bff9a3a96a5e74f40faa4db569
js <54337467@qq.com>
2022-01-19 20:24:58 +0800

    '模板引擎-过滤器'

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+// 注册dayjs的插件
+dayjs.extend(window.dayjs_plugin_relativeTime)
+
+// 设置模板引擎的过滤器
+template.defaults.imports.relativeTime = function (value) {
+  return dayjs().to(dayjs(value))
+}
+
 // 1. 发送请求，获取视频列表
 function loadData() {
   http
